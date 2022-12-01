8e3182a6e004fc42c661f07e4a2c5ffb563de890
erich-ika <erich.sabe.tudo@gmail.com>
2022-12-01 20:55:05 -0300

    Comparação com o banco de dados
    + Agora o sistema sabe se a senha digitada está certa!
    + A classe LoginBean que resolve isso.
    + ConnectionFactory ta no jeito pra todo mundo usar
    + e para usar o Connection Factory, o mysql-connector foi adicionado também

---
+++ src/java/br/com/nipem/connection/ConnectionFactory.java
@@ -0,0 +1,60 @@
+package br.com.nipem.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class ConnectionFactory {
+
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://db-autobot.c8g7b1rpw7ei.us-east-1.rds.amazonaws.com:3306/nipem";
+    private static final String USER = "admin";
+    private static final String PASS = "12345678";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (ClassNotFoundException | SQLException ex) {
+            javax.swing.JOptionPane.showMessageDialog(null, "Erro na conexão");
+            throw new RuntimeException("Erro na conexão: ", ex);
+        }
+    }
+
+    public static void closeConnection(Connection con) {
+        try {
+            if (con != null) {
+                con.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void closeConnection(Connection con, PreparedStatement stmt) {
+        closeConnection(con);
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void closeConnection(Connection con, PreparedStatement stmt, ResultSet rs) {
+        closeConnection(con, stmt);
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
--- src/java/br/com/nipem/security/LoginBean.java
+++ src/java/br/com/nipem/security/LoginBean.java
@@ -1,10 +1,20 @@
 package br.com.nipem.security;
 
+import br.com.nipem.connection.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class LoginBean {
 
-    private String login, type;
-    private char[] password;
-    
+    private String login, type, password;
+    PasswordAuthentication pa;
+
+    public LoginBean() {
+        this.pa = new PasswordAuthentication();
+    }
+
     public String getLogin() {
         return login;
     }
@@ -13,19 +23,57 @@ public class LoginBean {
         this.login = login;
     }
 
-    public char[] getPassword() {
+    public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
-        this.password = password.toCharArray();
+        this.password = password;
     }
 
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public void setType(String typeI) {
+        switch (typeI) {
+            case "Aluno":
+            case "SupervisorEstagio":
+            case "CoordenadorCurso":
+            case "InstituicaoDeEnsino":
+            case "Autarquia":
+                this.type = typeI;
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }
+
+    /*
+    public String getToken() {
+        return pa.hash(this.getPassword());
+    }
+     */
+    public String authenticate() {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        String sql = "select token from " + this.getType() + " where Cpf = ?";
+
+        String token;
+
+        try {
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, login);
+            rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                token = rs.getString(1);
+                return (pa.authenticate(password, token) ? "Senha certa!" : "Senha errada...") + "\n" + token + "\n" + password;
+            }
+            return "Não encontrado.";
+        } catch (SQLException ex) {
+            return "Erro:" + ex.getMessage();
+        }
     }
 }
