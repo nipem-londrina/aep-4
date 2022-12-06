5651c49fa77b28f9c889d37592d646f61725255b
erich-ika <erich.sabe.tudo@gmail.com>
2022-12-06 17:27:14 -0300

    Revert "Alterado o tipo do tipo de login de String para Int"
    This reverts commit 20bcea52201b993d102b0b4ee1f15360079b6878.

--- src/java/br/com/nipem/dao/SupervisorEstagioDAO.java
+++ src/java/br/com/nipem/dao/SupervisorEstagioDAO.java
@@ -10,17 +10,17 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class SupervisorEstagioDAO {
-
     Connection con;
-
     public SupervisorEstagioDAO() {
         con = ConnectionFactory.getConnection();
     }
-
+    
     public void cadastrar(SupervisorEstagio supervisorestagio) {
+        
         PreparedStatement stmt = null;
+        ResultSet rs = null;
         String sql = "INSERT INTO SupervisorEstagio(Nome,Cpf,Curso,Telefone,Ramal,Email,Token) VALUES(?, ?, ?, ?, ?, ?, ?)";
-
+        
         try {
             stmt = con.prepareStatement(sql);
             stmt.setString(1, supervisorestagio.getNome());
@@ -32,7 +32,9 @@ public class SupervisorEstagioDAO {
             stmt.setString(7, supervisorestagio.getToken());
             stmt.execute();
             stmt.close();
-
+            
+            
+            
         } catch (SQLException ex) {
             Logger.getLogger(SupervisorEstagioDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
